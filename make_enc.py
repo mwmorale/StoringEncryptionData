20ad798bdfc460294f81b1136a324fc5d5fcc92e
Max Morales <42706057+mwmorale@users.noreply.github.com>
2021-10-04 08:50:02 -0700

    Update make_enc.py
    Adding user password feature

--- make_enc.py
+++ make_enc.py
@@ -3,6 +3,14 @@ from my_library import bank
 from my_library import odd_chars
 
 
+print('\n-------------------------------------------')
+PW = input('CAREFULLY ENTER YOUR CHOSEN PASSWORD HERE: ')
+print('-------------------------------------------\n')
+
+
+
+PW_OFFSET = len(PW)
+
 def enc_motor(s):
     s = s.lower()
     lst = []
@@ -10,12 +18,12 @@ def enc_motor(s):
     for i in s:
         if( i in bank ):
             ind = bank.index(i)
-            if( ind<35 ):
-                i = bank[ind+1]
+            if( ind+PW_OFFSET < len(bank) ): #if rand<35 thennn i=bank[ ind+len(PW_OFFSET) ]
+                i = bank[ind+PW_OFFSET]
                 wrd = wrd+i
                 lst.append(i)
             else:
-                i = bank[0]
+                i = bank[ ind+PW_OFFSET-len(bank) ] #
                 wrd = wrd + i
                 lst.append(i)
         elif( i==' ' ):
@@ -31,6 +39,43 @@ def enc_motor(s):
 
 
 
+
+# ---------- PREVIOUS VERSION OF ENCRYPTION MOTOR -----------
+# def enc_motor(s):
+#     s = s.lower()
+#     lst = []
+#     wrd = ''
+#     for i in s:
+#         if( i in bank ):
+#             ind = bank.index(i)
+#             # rand = ind + len( PW_OFFSET )
+#             if( ind<35 ): #if rand<35 thennn i=bank[ ind+len(PW_OFFSET) ]
+#                 i = bank[ind+1]
+#                 wrd = wrd+i
+#                 lst.append(i)
+#             else:
+#                 i = bank[0] #
+#                 wrd = wrd + i
+#                 lst.append(i)
+#         elif( i==' ' ):
+#             i = random.choice(odd_chars)
+#             wrd = wrd + i
+#             lst.append(i)
+#         else:
+#             wrd = wrd + i
+#             #lst.append(i)
+#
+#     return wrd
+
+
+
+
+
+
+
+
+
+
 def dec_motor(d):
     d = d.lower()
     lst = []
@@ -38,12 +83,12 @@ def dec_motor(d):
     for i in d:
         if( i in bank ):
             ind = bank.index(i)
-            if( ind==0 ):
-                i = bank[ len(bank)-1 ]
+            if( ind-PW_OFFSET < 0 ):
+                i = bank[ len(bank) + (ind-PW_OFFSET) ]
                 wrd = wrd+i
                 lst.append(i)
             else:
-                i = bank[ind - 1]
+                i = bank[ind - PW_OFFSET]
                 wrd = wrd + i
                 lst.append(i)
         elif( i in odd_chars ):
@@ -55,3 +100,35 @@ def dec_motor(d):
             #lst.append(i)
 
     return wrd
+
+
+
+
+
+
+# -------------- PREVIOUS VERSION OF DECRYPTION MOTOR ------------------
+# def dec_motor(d):
+#     d = d.lower()
+#     lst = []
+#     wrd = ''
+#     for i in d:
+#         if( i in bank ):
+#             ind = bank.index(i)
+#             if( ind==0 ):
+#                 i = bank[ len(bank)-1 ]
+#                 wrd = wrd+i
+#                 lst.append(i)
+#             else:
+#                 i = bank[ind - 1]
+#                 wrd = wrd + i
+#                 lst.append(i)
+#         elif( i in odd_chars ):
+#             i = ' '
+#             wrd = wrd + i
+#             lst.append(i)
+#         else:
+#             wrd = wrd + i
+#             #lst.append(i)
+#
+#     return wrd
+
