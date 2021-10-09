dacd26ab67c454ac55fb7b44e9cedb45b229e88c
Max Morales <42706057+mwmorale@users.noreply.github.com>
2021-10-09 15:10:40 -0700

    Update my_library.py
    Adding GUI feature

--- my_library.py
+++ my_library.py
@@ -15,3 +15,31 @@ bank = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
 odd_chars = [ '\a', '\t', '\a' ]
 
 incorrect_input = '''\n\n\n\n\n\n\n\n\nWARNING: Incorrect input'''
+
+
+var = '''On your Desktop, a folder called "SecureData" has been created with "NOTES.txt" held inside
+<current drive> / <current user> / Desktop / SecureData / NOTES.txt 
+
+
+---- Creating a "SafeNote"? ----
+1. Find "NOTES.txt" and enter/alter contents
+2. Run this program(remember your password)
+3. DONE! The note is now unreadable 
+
+---- Viewing this "SafeNote"? ----
+1. Run this program(remember your password)
+2. Find "NOTES.txt" and enter/alter contents
+3. Run this program once again for security
+4. DONE! The note is now unreadable(again)
+
+Want to keep note safe after done? ENCRYPT!
+Locked/unreadable and want to view? DECRYPT!
+
+... put simply - run this program to open/decrypt and to close/encrypt
+
+
+
+***Do not edit the program files in any way
+***Do not edit its Desktop file structure in any way
+
+CLICK THE "DONE" BUTTON BELOW WHEN FINISHED'''
